d7efe17d14dad8d9f2b38d15111a836168beab97
Afzal Basha <afzalbasha992@gmail.com>
2023-04-18 14:16:42 +0530

    catalogue changes

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,7 @@ systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
 echo -e "\e[36m>>>>>>>Copy mongodb repo>>>>>>>>>>>>>>>>>\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp /root/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m>>>>>>>Install mongodb client>>>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org-shell -y
 echo -e "\e[36m>>>>>>>Load schema>>>>>>>>>>>>>>>>>\e[0m"
