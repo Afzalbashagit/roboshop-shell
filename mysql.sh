b0549db54d9092ac89ded55b92f84466ab6110e3
Afzal Basha <afzalbasha992@gmail.com>
2023-04-18 15:50:54 +0530

    mysql updates

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,11 @@
+echo -e "\e[36m>>>>>>>Disable mysql default version>>>>>>>>>>>>>>>>>\e[0m"
 dnf module disable mysql -y
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>Copy mysql repo>>>>>>>>>>>>>>>>>\e[0m"
+cp /root/roboshop-shell/mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "\e[36m>>>>>>>Install mysql>>>>>>>>>>>>>>>>>\e[0m"
 yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "\e[36m>>>>>>>Start mysql>>>>>>>>>>>>>>>>>\e[0m"
+systemctl enable mysq ld
+systemctl restart mysqld
+echo -e "\e[36m>>>>>>>Reset mysql password>>>>>>>>>>>>>>>>>\e[0m"
 mysql_secure_installation --set-root-pass RoboShop@1
-mysql -uroot -pRoboShop@1
