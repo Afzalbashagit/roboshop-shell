061f4d0c5944dfb39ffb0b47bf7768c9f5fa8126
Afzal Basha <afzalbasha992@gmail.com>
2023-04-18 15:31:46 +0530

    cart updates

--- cart.sh
+++ cart.sh
@@ -1,14 +1,21 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+echo -e "\e[36m>>>>>>>Installing NOdeJs>>>>>>>>>>>>>>>>>\e[0m"
 yum install nodejs -y
+echo -e "\e[36m>>>>>>>Add Application User>>>>>>>>>>>>>>>>>\e[0m"
 useradd roboshop
+echo -e "\e[36m>>>>>>>Creating app directory>>>>>>>>>>>>>>>>>\e[0m"
+rm -rf /app
 mkdir /app
+echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
 curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
 cd /app
+echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
 unzip /tmp/cart.zip
-cd /app
+echo -e "\e[36m>>>>>>>Install NOdeJs dependencies>>>>>>>>>>>>>>>>>\e[0m"
 npm install
-cp cart.service /etc/systemd/system/cart.service
+echo -e "\e[36m>>>>>>>Copy cart systemd file>>>>>>>>>>>>>>>>>\e[0m"
+cp /root/roboshop-shell/cart.service /etc/systemd/system/cart.service
+echo -e "\e[36m>>>>>>>start cart service>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable cart
-systemctl start cart
+systemctl restart cart
 
