152e714e86bd8d0410807da433b2a3a28bfee08b
Afzal Basha <afzalbasha992@gmail.com>
2023-04-18 15:07:52 +0530

    redis updates

--- redis.sh
+++ redis.sh
@@ -4,7 +4,7 @@ yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 echo -e "\e[36m>>>>>>>Installing redis>>>>>>>>>>>>>>>>>\e[0m"
 dnf module enable redis:remi-6.2 -y
 yum install redis -y
-echo -e "\e[36m>>>>>>>Installing redis>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[36m>>>>>>>update listening address>>>>>>>>>>>>>>>>>\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0' /etc/redis.conf /etc/redis/redis.conf
 echo -e "\e[36m>>>>>>>Starting redis>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable redis
