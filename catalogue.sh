ef8ed5db64f52efff3bf85be3c06bde3af99d2ae
Afzal Basha <afzalbasha992@gmail.com>
2023-04-28 18:20:03 +0530

    cart changes

--- catalogue.sh
+++ catalogue.sh
@@ -2,27 +2,8 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
 echo -e "\e[36m>>>>>>>Configuring nodejs repos>>>>>>>>>>>>>>>>>\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[36m>>>>>>>Installing NOdeJs>>>>>>>>>>>>>>>>>\e[0m"
-yum install nodejs -y
-echo -e "\e[36m>>>>>>>Add Application User>>>>>>>>>>>>>>>>>\e[0m"
-useradd {app_user}
-echo -e "\e[36m>>>>>>>Creating app directory>>>>>>>>>>>>>>>>>\e[0m"
-rm -rf /app
-mkdir /app
-echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
-unzip /tmp/catalogue.zip
-echo -e "\e[36m>>>>>>>Install NOdeJs dependencies>>>>>>>>>>>>>>>>>\e[0m"
-npm install
-echo -e "\e[36m>>>>>>>Copy catalogue systemd file>>>>>>>>>>>>>>>>>\e[0m"
-cp /root/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
-echo -e "\e[36m>>>>>>>start catalogue service>>>>>>>>>>>>>>>>>\e[0m"
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+component=catalogue
+func_nodejs
 echo -e "\e[36m>>>>>>>Copy mongodb repo>>>>>>>>>>>>>>>>>\e[0m"
 cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m>>>>>>>Install mongodb client>>>>>>>>>>>>>>>>>\e[0m"
