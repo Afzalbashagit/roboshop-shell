89183bce6e20aa0876516e3a25db38f569a8127e
Afzal Basha <afzalbasha992@gmail.com>
2023-04-28 11:54:44 +0530

    payment updates

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,14 @@
+echo -e "\e[36m>>>>>>>Install nginx>>>>>>>>>>>>>>>>>\e[0m"
 yum install nginx -y
+echo -e "\e[36m>>>>>>>copy content>>>>>>>>>>>>>>>>>\e[0m"
 cp frontend.confo /etc/nginx/default.d/roboshop.conf
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[36m>>>>>>>remove existed file in /usr/share/nginx/html>>>>>>>>>>>>>>>>>\e[0m"
 rm -rf /usr/share/nginx/html/*
+echo -e "\e[36m>>>>>>>download content>>>>>>>>>>>>>>>>>\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+echo -e "\e[36m>>>>>>>unzip content>>>>>>>>>>>>>>>>>\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
+echo -e "\e[36m>>>>>>>start system services>>>>>>>>>>>>>>>>>\e[0m"
+systemctl enable nginx
 systemctl restart nginx
--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,9 @@
+echo -e "\e[36m>>>>>>>copy  content>>>>>>>>>>>>>>>>>\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+echo -e "\e[36m>>>>>>>Configuring nodejs repos>>>>>>>>>>>>>>>>>\e[0m"
 yum install mongodb-org -y
+echo -e "\e[36m>>>>>>>update address in mongod file>>>>>>>>>>>>>>>>>\e[0m"
 sed -i -e 's|127.0.0.1|0.0.0.0|' /etc/mongod.conf
+echo -e "\e[36m>>>>>>>Start system services>>>>>>>>>>>>>>>>>\e[0m"
 systemctl enable mongod
 systemctl restart mongod
