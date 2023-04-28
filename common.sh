9ec2446fdf3e782ac1507c60dd38690818e6560b
Afzal Basha <afzalbasha992@gmail.com>
2023-04-28 18:14:42 +0530

    cart changes

--- common.sh
+++ common.sh
@@ -12,16 +12,16 @@ func_nodejs(){
   rm -rf /app
   mkdir /app
   echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
-  curl -o /tmp/{component}.zip https://roboshop-artifacts.s3.amazonaws.com/{component}.zip
+  curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
   cd /app
   echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
-  unzip /tmp/{component}.zip
+  unzip /tmp/${component}.zip
   echo -e "\e[36m>>>>>>>Install NOdeJs dependencies>>>>>>>>>>>>>>>>>\e[0m"
   npm install
   echo -e "\e[36m>>>>>>>Copy cart systemd file>>>>>>>>>>>>>>>>>\e[0m"
-  cp $script_path/{component}.service /etc/systemd/system/{component}.service
+  cp $script_path/${component}.service /etc/systemd/system/${component}.service
   echo -e "\e[36m>>>>>>>start cart service>>>>>>>>>>>>>>>>>\e[0m"
   systemctl daemon-reload
-  systemctl enable {component}
-  systemctl restart {component}
+  systemctl enable ${component}
+  systemctl restart ${component}
 }
