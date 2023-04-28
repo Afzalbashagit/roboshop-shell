7ff127b0e34747f0c1dea79f8b55ed7afdc4f7b9
Afzal Basha <afzalbasha992@gmail.com>
2023-04-28 18:33:57 +0530

    changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,8 +1,27 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-component=catalogue
-func_nodejs
+  echo -e "\e[36m>>>>>>>Installing NOdeJs>>>>>>>>>>>>>>>>>\e[0m"
+  yum install nodejs -y
+  echo -e "\e[36m>>>>>>>Add Application User>>>>>>>>>>>>>>>>>\e[0m"
+  useradd {app_user}
+  echo -e "\e[36m>>>>>>>Creating app directory>>>>>>>>>>>>>>>>>\e[0m"
+  rm -rf /app
+  mkdir /app
+  echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
+  curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+  cd /app
+  echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
+  unzip /tmp/catalogue.zip
+  echo -e "\e[36m>>>>>>>Install NOdeJs dependencies>>>>>>>>>>>>>>>>>\e[0m"
+  npm install
+  echo -e "\e[36m>>>>>>>Copy cart systemd file>>>>>>>>>>>>>>>>>\e[0m"
+  cp $script_path/catalogue.service /etc/systemd/system/catalogue.service
+  echo -e "\e[36m>>>>>>>start cart service>>>>>>>>>>>>>>>>>\e[0m"
+  systemctl daemon-reload
+  systemctl enable catalogue
+  systemctl restart catalogue
+
 echo -e "\e[36m>>>>>>>Copy mongodb repo>>>>>>>>>>>>>>>>>\e[0m"
 cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m>>>>>>>Install mongodb client>>>>>>>>>>>>>>>>>\e[0m"
