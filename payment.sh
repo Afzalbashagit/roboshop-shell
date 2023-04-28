7356b1067fb3266741066ed85680e923db873932
Afzal Basha <afzalbasha992@gmail.com>
2023-04-28 18:00:09 +0530

    changes

--- payment.sh
+++ payment.sh
@@ -15,10 +15,10 @@ cd /app
 unzip /tmp/payment.zip
 echo -e "\e[36m>>>>>>>Install python dependencies>>>>>>>>>>>>>>>>>\e[0m"
 pip3.6 install -r requirements.txt
-echo -e "\e[36m>>>>>>>Add application user in rabbitmq>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[36m>>>>>>>start payment services>>>>>>>>>>>>>>>>>\e[0m"
 sed -i -e "s|rabbitmq_appuser_password|${rabbitmq_appuser_password}|" $script_path/payment.service
 cp $script_path/payment.service /etc/systemd/system/payment.service
-echo -e "\e[36m>>>>>>>start payment services>>>>>>>>>>>>>>>>>\e[0m"
+echo -e "\e[36m>>>>>>>start system services>>>>>>>>>>>>>>>>>\e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl start payment
