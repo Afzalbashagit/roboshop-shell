9448936f56f43530cb95a3c3fdf74d01ca9e6e58
Afzal Basha <afzalbasha992@gmail.com>
2023-04-28 18:22:35 +0530

    cart changes

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,6 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[36m>>>>>>>Configuring nodejs repos>>>>>>>>>>>>>>>>>\e[0m"
 component=catalogue
 func_nodejs
 echo -e "\e[36m>>>>>>>Copy mongodb repo>>>>>>>>>>>>>>>>>\e[0m"
