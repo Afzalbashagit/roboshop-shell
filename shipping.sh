99e52b06ca22704c7b5af970c80a61c516671653
Afzal Basha <afzalbasha992@gmail.com>
2023-04-29 11:14:42 +0530

    changes

--- shipping.sh
+++ shipping.sh
@@ -6,6 +6,7 @@ mysql_root_password=$1
 if [ -z "$mysql_root_password" ]
 then
   echo Input mysql password is missing
+  exit
 fi
 echo -e "\e[36m>>>>>>>Install maven>>>>>>>>>>>>>>>>>\e[0m"
 yum install maven -y
