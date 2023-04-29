037afcef8b89ff02bba85db2259c8ead23a91493
Afzal Basha <afzalbasha992@gmail.com>
2023-04-29 09:51:32 +0530

    changes

--- user.sh
+++ user.sh
@@ -1,26 +1,9 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[36m>>>>>>>Installing NOdeJs>>>>>>>>>>>>>>>>>\e[0m"
-yum install nodejs -y
-echo -e "\e[36m>>>>>>>Add Application User>>>>>>>>>>>>>>>>>\e[0m"
-useradd {app_user}
-echo -e "\e[36m>>>>>>>Creating app directory>>>>>>>>>>>>>>>>>\e[0m"
-rm -rf /app
-mkdir /app
-echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
-unzip /tmp/user.zip
-echo -e "\e[36m>>>>>>>Install NOdeJs dependencies>>>>>>>>>>>>>>>>>\e[0m"
-npm install
-echo -e "\e[36m>>>>>>>Copy user systemd file>>>>>>>>>>>>>>>>>\e[0m"
-cp $script_path/user.service /etc/systemd/system/user.service
-echo -e "\e[36m>>>>>>>start user service>>>>>>>>>>>>>>>>>\e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl restart user
+
+component=user
+func_nodejs
 echo -e "\e[36m>>>>>>>Copy mongodb repo>>>>>>>>>>>>>>>>>\e[0m"
 cp $script_path/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[36m>>>>>>>Install mongodb client>>>>>>>>>>>>>>>>>\e[0m"
