01fe3afc222d12a748929ce290d29a3b7ed3339f
Afzal Basha <afzalbasha992@gmail.com>
2023-04-29 10:03:38 +0530

    changes

--- common.sh
+++ common.sh
@@ -2,24 +2,29 @@ script=$(realpath "$0")
 script_path=$(dirname "$script")
 app_user=roboshop
 
+
+func_printhead(){
+    echo -e "\e[36m>>>>>>>$1>>>>>>>>>>>>>>>>>\e[0m
+}
+
 func_nodejs(){
-    echo -e "\e[36m>>>>>>>Installing NOdeJs>>>>>>>>>>>>>>>>>\e[0m"
+ func_printhead "Installing NOdeJs"
     yum install nodejs -y
-    echo -e "\e[36m>>>>>>>Add Application User>>>>>>>>>>>>>>>>>\e[0m"
+func_printhead " Add Application User"
     useradd {app_user}
-    echo -e "\e[36m>>>>>>>Creating app directory>>>>>>>>>>>>>>>>>\e[0m"
+  func_printhead " Creating app directory"
     rm -rf /app
     mkdir /app
-    echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
+   func_printhead "Download app content"
     curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
     cd /app
-    echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
+   func_printhead "unzip app content"
     unzip /tmp/${component}.zip
-    echo -e "\e[36m>>>>>>>Install NOdeJs dependencies>>>>>>>>>>>>>>>>>\e[0m"
+ func_printhead "Install NOdeJs dependencies"
     npm install
-    echo -e "\e[36m>>>>>>>Copy cart systemd file>>>>>>>>>>>>>>>>>\e[0m"
+   func_printhead "Copy cart systemd file"
     cp $script_path/${component}.service /etc/systemd/system/${component}.service
-    echo -e "\e[36m>>>>>>>start cart service>>>>>>>>>>>>>>>>>\e[0m"
+   func_printhead "start cart service"
     systemctl daemon-reload
     systemctl enable ${component}
     systemctl restart ${component}
