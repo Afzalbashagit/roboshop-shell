e408ffcf7ff004726bda654e0047ecafaf6cec92
Afzal Basha <afzalbasha992@gmail.com>
2023-04-30 13:05:43 +0530

    changes

--- common.sh
+++ common.sh
@@ -19,6 +19,36 @@ func_schema(){
   func_print "Load schema"
   mongo --host  mongodb-dev.afzalbasha.cloud</app/schema/${component}.js
  fi
+  if [ "$schema_setup" == "mysql" ];then
+    func_print "Install mysql"
+    yum install mysql -y
+    func_print "Load schema"
+    mysql -h mysql-dev.afzalbasha.cloud> -uroot -p${mysql_root_password} < /app/schema/${component}.sql
+  fi
+
+}
+
+
+func_app_prereq(){
+   func_print "add application user"
+    useradd {app_user}
+    func_print "create app directory"
+    rm -rf /app
+    mkdir /app
+    func_print "Download app content"
+    curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+    func_print "unzip app content"
+    cd /app
+    unzip /tmp/${component}.zip
+}
+func_systemd_service()
+{
+    func_print "setup systemd  service"
+    cp $script_path/${component}.service /etc/systemd/system/${component}.service
+    func_print "start systemd service"
+    systemctl daemon-reload
+    systemctl enable ${component}
+    systemctl restart ${component}
 }
 
 func_nodejs(){
@@ -44,4 +74,16 @@ func_nodejs(){
     systemctl restart ${component}
 
     func_schema
+}
+
+func_java(){
+   func_print "Install maven"
+  yum install maven -y
+ func_app_prereq
+  func_print "Install maven dependencies"
+  mvn clean package
+  mv target/${component}-1.0.jar ${component}.jar
+
+ func_schema
+ func_systemd_service
 }
--- shipping.sh
+++ shipping.sh
@@ -8,28 +8,8 @@ then
   echo Input mysql password is missing
   exit
 fi
-echo -e "\e[36m>>>>>>>Install maven>>>>>>>>>>>>>>>>>\e[0m"
-yum install maven -y
-echo -e "\e[36m>>>>>>>add application user>>>>>>>>>>>>>>>>>\e[0m"
-useradd {app_user}
-echo -e "\e[36m>>>>>>>create app directory>>>>>>>>>>>>>>>>>\e[0m"
-rm -rf /app
-mkdir /app
-echo -e "\e[36m>>>>>>>Download app content>>>>>>>>>>>>>>>>>\e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-echo -e "\e[36m>>>>>>>unzip app content>>>>>>>>>>>>>>>>>\e[0m"
-cd /app
-unzip /tmp/shipping.zip
-echo -e "\e[36m>>>>>>>Install maven dependencies>>>>>>>>>>>>>>>>>\e[0m"
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[36m>>>>>>>Install mysql>>>>>>>>>>>>>>>>>\e[0m"
-yum install mysql -y
-echo -e "\e[36m>>>>>>>>>>Load schema>>>>>>>>>>>>>>\e[0m"
-mysql -h mysql-dev.afzalbasha.cloud> -uroot -p${mysql_root_password} < /app/schema/shipping.sql
-echo -e "\e[36m>>>>>>>setup systemd shipping service>>>>>>>>>>>>>>>>>\e[0m"
-cp $script_path/shipping.service /etc/systemd/system/shipping.service
-echo -e "\e[36m>>>>>>>start shipping service>>>>>>>>>>>>>>>>>\e[0m"
-systemctl daemon-reload
-systemctl enable shipping
-systemctl restart shipping
+
+component=java
+
+schema_setup=mysql
+func_java
