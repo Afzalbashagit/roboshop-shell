031bb765dd395100dd7aefdd28f00dc9ac294469
Afzal Basha <afzalbasha992@gmail.com>
2023-04-30 14:40:21 +0530

    changes

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,17 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-echo -e "\e[36m>>>>>>>Install nginx>>>>>>>>>>>>>>>>>\e[0m"
-yum install nginx -y
-echo -e "\e[36m>>>>>>>copy content>>>>>>>>>>>>>>>>>\e[0m"
-cp frontend.confo /etc/nginx/default.d/roboshop.conf
-echo -e "\e[36m>>>>>>>remove existed file in /usr/share/nginx/html>>>>>>>>>>>>>>>>>\e[0m"
+func_print "Install nginx"
+yum install nginx -y &>>log_file
+func_print"copy content"
+cp frontend.confo /etc/nginx/default.d/roboshop.conf &>>log_file
+func_print "remove existed file in /usr/share/nginx/html"
 rm -rf /usr/share/nginx/html/*
-echo -e "\e[36m>>>>>>>download content>>>>>>>>>>>>>>>>>\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e[36m>>>>>>>unzip content>>>>>>>>>>>>>>>>>\e[0m"
+func_print "download content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
+func_print "unzip content"
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-echo -e "\e[36m>>>>>>>start system services>>>>>>>>>>>>>>>>>\e[0m"
-systemctl enable nginx
-systemctl restart nginx
+unzip /tmp/frontend.zip &>>log_file
+func_print "start system services"
+systemctl enable nginx &>>log_file
+systemctl restart nginx &>>log_file
